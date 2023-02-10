ac3b48b9fa488bb302fff7a626871686f43879eb
adm68 <125032820+adm68@users.noreply.github.com>
2023-02-10 15:33:59 +0100

    Add files via upload

---
+++ coolOrange_CandidateChallenge-master/coolOrange_CandidateChallenge-master/Project/coolOrange_CandidateChallenge/Array.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace coolOrange_CandidateChallenge
+{
+    public class Array
+    {
+        public static void printIntArray(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]);
+                if (i != a.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static void printIntMatrix(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for(int i1 = 0; i1 < a.GetLength(1); i1++)
+                {
+                    Console.Write(a[i, i1]);
+                    if (i != a.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                if (i + 1 < a.GetLength(0))
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+
+
+        public static int FindMaxValue(int[] array, int position1, int position2)
+        {
+            int ret;
+            if (position1 < 0 || position2 < 0 || position2 < position1)
+                {
+                ret = -1;
+                return ret;
+            }
+            else
+            {
+                ret = array[position1];
+                for (int i = position1 + 1; i <= position2; i++)
+                {
+                    if (array[i] > ret)
+                    {
+                        ret = array[i];
+                    }
+                }
+                return ret;
+            }
+        }	
+
+        public static int FindMinPosition(int[] array, int position1, int position2)
+        {
+            int ret;
+            if (position1 < 0 || position2 < 0 || position2 < position1 || position1 >= array.Length || position2 >= array.Length)
+            {
+                ret = -1;
+                return ret;
+            }
+            else
+            {
+                ret = position1;
+                for (int i = position1 + 1; i < position2; i++)
+                {
+                    if (array[i] < array[ret])
+                    {
+                        ret = i;
+                    }
+                }
+                return ret;
+            }
+        }
+
+        public static void Swap(int[] array, int position1, int position2)
+        {
+            if (position1 < 0 || position2 < 0 || position1 >= array.Length || position2 >= array.Length)
+            {
+                Console.WriteLine("beide Positionen muessen im Array enthalten sein");
+            }
+            else
+            {
+                (array[position1], array[position2]) = (array[position2], array[position1]);
+            }
+        }
+
+        public static void ShiftLeftByOne(int[] array, int position1, int position2)
+        {
+            if (position1 < 0 || position2 < 0 || position1 >= array.Length || position2 >= array.Length || position2 < position1)
+            {
+                Console.WriteLine("beide Positionen muessen im Array enthalten sein");
+            }
+            else
+            {
+                for(int i = position1; i < position2 - 1; i++)
+                {
+                    (array[i], array[i + 1]) = (array[i + 1], array[i]);
+                }
+            }
+
+
+        }
+
+        public static int[] CreateRandomArray(int size, int minValue, int maxValue)
+        {
+            int[] ret = new int[size];
+            Random rnd = new Random();
+            for(int i = 0; i < size; i++)
+            {
+                ret[i] = rnd.Next(minValue, maxValue);
+            }
+            return ret;
+        }
+
+        public static int[,] CreateRandomMatrix(int rows, int columns, int minValue, int maxValue)
+        {
+            int[,] ret = new int[rows, columns];
+            Random rnd = new Random();
+            for(int i = 0; i < rows; i++)
+            {
+                for(int i1 = 0; i1 < columns; i1++)
+                {
+                    ret[i, i1] = rnd.Next(minValue, maxValue);
+                }
+            }
+            return ret;
+        }
+
+        public static int[,] CopyArray(int[] array)
+        {
+            int[,] ret = new int[2, array.Length];
+            for(int i = 0; i < 2; i++)
+            {
+                for(int i1 = 0; i1 < array.Length; i1++)
+                {
+                    ret[i, i1] = array[i1];
+                }
+            }
+            return ret;
+        }
+
+        public static int FindInSortedArray(int[] array, int number)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int ret = (left + right) / 2;
+                if (array[ret] == number)
+                {
+                    return ret;
+                }
+                else if (array[ret] < number)
+                {
+                    left = ret + 1;
+                }
+                else
+                {
+                    right = ret - 1;
+                }
+            }
+
+            return -1;
+        }
+
+
+
+    }
+}
---
+++ coolOrange_CandidateChallenge-master/coolOrange_CandidateChallenge-master/Project/coolOrange_CandidateChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coolOrange_CandidateChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] {2, 3, 5, 4, 1};
+            Console.Write("int[] a = ");
+            Array.printIntArray(a);
+            Console.WriteLine("FindMaxValue(a, 0, 1): " + Array.FindMaxValue(a, 0, 1));
+            Console.WriteLine("FindMinValue(a, 1, 3): " + Array.FindMinPosition(a, 1, 3));
+            Array.Swap(a, 0, 2);
+            Console.Write("Swap:  ");
+            Array.printIntArray(a);
+            Array.ShiftLeftByOne(a, 1, 4);
+            Console.Write("ShiftLeftByOne:  ");
+            Array.printIntArray(a);
+            Console.WriteLine("CreateRandomArray(10, 1, 10): ");
+            Array.printIntArray(Array.CreateRandomArray(10, 1, 10));
+            Console.WriteLine("CreateRandomMatrix(5, 10, 1, 10): ");
+            Array.printIntMatrix(Array.CreateRandomMatrix(5, 10, 1, 10));
+            int[,] a1 = Array.CopyArray(a);
+            Console.WriteLine("CopyArray(a): ");
+            Array.printIntMatrix(a1);
+            int[] a2 = new int[5] {1, 2, 3, 4, 5};
+            Console.Write("int[] a2 = ");
+            Array.printIntArray(a2);
+            Console.WriteLine("FindInSortedArray(a2, 4) = " + Array.FindInSortedArray(a2, 4));
+            Console.WriteLine("IsPalindrome(\"racecar\")" + PalindromeChecker.IsPalindrome("racecar"));
+            Console.WriteLine("IsPalindrome(\"hello\")" + PalindromeChecker.IsPalindrome("hello"));
+
+
+
+
+
+
+        }
+    }
+}
